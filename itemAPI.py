fd396ab7cdaf1d834ec6b1e804b92b0cc416106d
Holden Schaffer <holdenschaffer@gatech.edu>
2019-10-26 13:04:03 -0400

    Add: Disease field for item

--- itemAPI.py
+++ itemAPI.py
@@ -4,12 +4,12 @@ def createItemDictFromCSV():
     items = {}
     df = pd.read_csv("Products.csv")
     for index, row in df.iterrows():
-        items[row['Name']] = Item(row['Name'], row['Price'], row['Description'], row['Link'])
+        items[row['Name']] = Item(row['Name'], row['Price'], row['Description'], row['Link'], row['Disease'])
     return items
 
-def newItemToCSV(name, price, description, link):
+def newItemToCSV(name, price, description, link, disease):
     df = pd.read_csv("Products.csv")
-    df.loc[len(df)] = [name, price, description, link]
+    df.loc[len(df)] = [name, price, description, link, disease]
     df.to_csv('Products.csv', index=False)
 
 def delItemFromCSV(name):
@@ -18,9 +18,9 @@ def delItemFromCSV(name):
     df.to_csv('Products.csv', index=False)
               
 class Item:
-    def __init__(self, name, price, description, link):
+    def __init__(self, name, price, description, link, disease):
         self.name = name
         self.price = price
         self.description = description
         self.link = link
-        
+        self.disease = disease
