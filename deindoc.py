41133056b388a6c4b9cd2b13008b4fa74f1f5f55
Arjun Karanam <akaranam@stanford.edu>
2019-10-25 23:27:41 -0400

    pages

--- deindoc.py
+++ deindoc.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return "<h1>Your Problem</h1>"
 
 
-@app.route("/about")
+@app.route("/shop")
 def about():
-    return "<h1>About Page</h1>"
+    return "<h1>Marketplace</h1>"
 
 
 if __name__ == '__main__':
