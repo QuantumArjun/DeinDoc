ee5399be09d1700a1913cd2b7572d1e45dde41e6
Arjun Karanam <akaranam@stanford.edu>
2019-10-26 23:14:46 -0400

    Added Big API bois

--- deindoc.py
+++ deindoc.py
@@ -11,23 +11,24 @@ model = pickle.load(open('model.sav', 'rb'))
 
 cartList = []
 
-api_token = "gAAAAMA_MWOas38X6QqzABin1xvPd5RiCaDTYTzbjcJlGVhDO8zfEFAIs_F4OM_KTPhhhg75uZYLEXEys3BtdzGCU5awJLEx6y86yEQ4DbuHc0eygkQqNe_IVzRLB2y_kz72eaYNg5Gmb1Nyo6XCcTtNTu7o9oWAdMY9TFP490lRtati9AAAAIAAAABLQmFuEkaDIEHkuosYILnK5nV1XE647K1h7cvzx5Fai7R3rsWAImg-PKoW8Sahyj2nge7DDEhC0FFs8BC8NJFNgtiAEowuECD6ZJ88ze66ma-qpe2RgBdey8WdVjl7LTY-JYoEGhPyT21yzUpFgl_1r8QXKd7UgA_vXosFJC2vMBb_bgrtScAiVLD8oL6MPFAFyTkVR7H4Dk_s3zCsO6gTQlmT8cIjn86kW6t-p8ANySY7uFAsHNqib5qHz-_SHxVlhiusFZdl0bjQxNk3y-sEmpqTe7rxzke7fzenADutQKuqr2ONexylvlUvLhbY0"
-client_id = "gt_552463"
-client_secret = "004e007a-004a-0035-4b00-740033006800"
 
-headers = {'Authorization': 'Bearer {0}'.format(api_token), 'Accept': 'application/json'}
+url = "https://api-reg-apigee.ncrsilverlab.com/v2/orders"
 
+querystring = {"store_number":"1"}
 
-conn = http.client.HTTPSConnection("api-reg.ncrsilverlab.com")
+payload = "{\n    \n  \"Orders\": [\n    {\n      \"IsClosed\": true,\n      \"OrderNumber\": \"string\",\n      \"OrderDateTime\": \"2019-10-27T00:38:56.555Z\",\n      \"OrderDueDateTime\": \"2019-10-27T00:38:56.555Z\",\n      \"IsPaid\": true,\n      \"Customer\": {\n        \"CustomerId\": 0,\n        \"CustomerName\": \"string\",\n        \"Email\": \"string\",\n        \"PhoneNumber\": \"string\",\n        \"Address1\": \"string\",\n        \"Address2\": \"string\",\n        \"Address3\": \"string\",\n        \"City\": \"string\",\n        \"State\": \"string\",\n        \"ZipCode\": \"string\"\n      },\n      \"CustomerId\": 0,\n      \"CustomerName\": \"string\",\n      \"Email\": \"string\",\n      \"PhoneNumber\": \"string\",\n      \"TableReference\": \"string\",\n      \"TaxAmount\": 0,\n      \"TipAmount\": 0,\n      \"LineItems\": [\n        {\n          \"ExternalItemId\": \"1710184\",\n        \n          \"ItemName\": \"string\",\n          \"Quantity\": 1,\n          \"UnitPrice\": 0,\n          \"UnitSellPrice\": 0,\n          \"ExtendedSellPrice\": 0,\n          \n          \"Notes\": [\n            \"string\"\n          ],\n          \"BagName\": \"string\"\n        }\n      ],\n      \"Notes\": [\n        \"string\"\n      ],\n      \"KitchenLeadTimeInMinutes\": 0,\n      \"SkipReceipt\": true,\n      \"SkipKitchen\": true\n    }\n  ],\n  \"SourceApplicationName\": \"string\"\n}"
+headers = {
+    'Accept': "application/json",
+    'Content-Type': "application/json",
+    'Host': "api-reg-apigee.ncrsilverlab.com",
+    'Authorization': "Bearer gAAAAMA_MWOas38X6QqzABin1xvPd5RiCaDTYTzbjcJlGVhDO8zfEFAIs_F4OM_KTPhhhg75uZYLEXEys3BtdzGCU5awJLEx6y86yEQ4DbuHc0eygkQqNe_IVzRLB2y_kz72eaYNg5Gmb1Nyo6XCcTtNTu7o9oWAdMY9TFP490lRtati9AAAAIAAAABLQmFuEkaDIEHkuosYILnK5nV1XE647K1h7cvzx5F_ai7R3rsWAImg-PKoW8Sahyj2nge7DDEhC0FFs8BC8NJFNgtiAEowuECD6ZJ88ze66ma_-qpe2RgBdey8WdVjl7LTY-JYoEGhPyT21yzUpFgl_1r8QXKd7UgA_vXosFJC2vMBb_bgrtScAiVLD8oL6MPFAFyTkVR7H4Dk_s3zCsO6gTQlmT8cIjn86kW6t-p8ANySY7uFAsHNqib5qHz-_SHxVlhiusFZdl0bjQxNk3y-sEmpqTe7rxzke7fzenADutQKuqr2ONexylvlUvLhbY0",
+    'cache-control': "no-cache",
+    'Postman-Token': "40677c10-b4da-4d85-acf9-8791f890e36f"
+    }
 
-payload = "{\"Orders\": [{\"IsClosed\":true,\"OrderNumber\":\"string\",\"OrderDateTime\":\"2019-10-27T00:38:56.555Z\",\"OrderDueDateTime\":\"2019-10-27T00:38:56.555Z\",\"IsPaid\":true,\"Customer\":{\"CustomerId\":0,\"CustomerName\":\"string\",\"Email\":\"string\",\"PhoneNumber\":\"string\",\"Address1\":\"string\",\"Address2\":\"string\",\"Address3\":\"string\",\"City\":\"string\",\"State\":\"string\",\"ZipCode\":\"string\"},\"CustomerId\":0,\"CustomerName\":\"string\",\"Email\":\"string\",\"PhoneNumber\":\"string\",\"TableReference\":\"string\",\"TaxAmount\": 0,\"TipAmount\": 0,\"LineItems\":[{\"ExternalItemId\":\"1710184\",\"ItemName\":\"string\",\"Quantity\":1,\"UnitPrice\":0,\"UnitSellPrice\":0,\"ExtendedSellPrice\":0,\"Notes\":[\"string\"],\"BagName\":\"string\"}],\"Notes\":[\"string\"],\"KitchenLeadTimeInMinutes\":0,\"SkipReceipt\":true,\"SkipKitchen\":true,]}],\"SourceApplicationName\":\"string\"}"
+response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
 
-conn.request("POST", "/orders", payload, headers)
-
-res = conn.getresponse()
-data = res.read()
-
-print(data.decode("utf-8"))
+print(response.text)
 
 
 app.config['SECRET_KEY'] = 'dave'
