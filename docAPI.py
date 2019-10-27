9c67d0e56295af6ec4392c83e8d70093faf55205
HunterSchaffer <43712911+HunterSchaffer@users.noreply.github.com>
2019-10-27 01:42:21 -0400

    doctors work

--- docAPI.py
+++ docAPI.py
@@ -6,14 +6,15 @@ def createDocDictFromCSV():
     items = {}
     df = pd.read_csv("Doctors.csv")
     for index, row in df.iterrows():
-        items[row['Name']] = Doc(row['Name'], row['Rating'], row['Link'], row['Specialty'], row['Contact'])
+        items[row['Name']] = Doc(row['Name'], row['Rating'], row['Link'], row['Specialty'], row['Contact'], row['Doc_id'])
     return items
 
 
 class Doc:
-    def __init__(self, name, rating, link, specialty, contact):
+    def __init__(self, name, rating, link, specialty, contact,doc_id):
         self.name = name
         self.rating = rating
         self.link = link
         self.specialty = specialty
         self.contact = contact
+        self.doc_id = doc_id
